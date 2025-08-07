b9bfcf31534543b8b790af73c4e5fa7096524a0a
RashedujjamanNoor <noorrk042@gmail.com>
2025-08-07 23:47:39 +0600

    create userRouter and controller

---
+++ controllers/userController.js
@@ -0,0 +1,5 @@
+const register = async (req, res) => {
+  console.log("i am register controller");
+};
+
+export { register };
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import userRouter from "./routes/userRouter.js";
 
 dotenv.config();
 const app = express();
@@ -9,5 +10,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/user", userRouter);
 
 app.listen(port, (req, res) => console.log(`app is running at port ${port}`));
